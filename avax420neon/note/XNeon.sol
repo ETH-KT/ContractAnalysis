e5a20c5802874cd420de919fed8acfe9c6c00dcc
Blockchain-Key <1416893908@qq.com>
2024-01-18 11:18:49 +0800

    add XNeon note

---
+++ avax420neon/note/XNeon.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @dev
+ * _mint(msg.sender, 21000000 * 10 ** decimals());总供应量21000000个
+ * @notice 项目方代币，无法增发，固定总量21000000个，目前47.84%进入LBPair池子,45.23%归项目方所有，剩下归玩家所有，
+ * 游戏还没推出，目前仅能通过池子购买和质押Land和AVAX420获取
+ */
+contract XNeon is ERC20, Ownable {
+    constructor() ERC20("XNeon", "XNeon") {
+        _mint(msg.sender, 21000000 * 10 ** decimals());
+    }
+}
