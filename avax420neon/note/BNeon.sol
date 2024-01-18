8bbfbc80281ea0aed5978f969a4280674d751dd0
Blockchain-Key <1416893908@qq.com>
2024-01-18 11:18:35 +0800

    add BNeon note

---
+++ avax420neon/note/BNeon.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @dev
+ *  _mint(msg.sender, 100000000 * 10 ** decimals());预铸造100000000个
+ *  mintAdditionalTokens;可无限增发,
+ * @notice 作为游戏代币，只在游戏内流通，无限增发也能说得过去
+ */
+contract BNeon is ERC20, Ownable {
+    constructor() ERC20("BNeon", "BNeon") {
+        _mint(msg.sender, 100000000 * 10 ** decimals());
+    }
+
+    function mintAdditionalTokens(uint256 amount) public onlyOwner {
+        _mint(msg.sender, amount);
+    }
+}
